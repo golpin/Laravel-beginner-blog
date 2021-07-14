b23f1b850e5ccb1be996be5f3c983ef360ca9507
golpin <ttmessage1976@gmail.com>
2021-07-14 21:22:48 +0900

    userとadminのルート、コントローラー、ビューの調整

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
@@ -20,18 +20,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware('auth:admin')->group(function(){
-        
-});
-
-
-//Route::get('/', function () {
-//    return view('welcome');
+//Route::middleware('auth:admin')->group(function(){
+//    Route::get('index', [AdminController::class, 'Index'])->name('index');
 //});
 
+
+Route::get('/', function () {
+    return view('admin.welcome');
+});
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('admin.dashboard');
+})->middleware(['auth:admin'])->name('dashboard');
 
 //Route::get('/register', [RegisteredUserController::class, 'create'])
 //                ->middleware('guest')
@@ -64,25 +64,25 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.update');
 
 Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('verification.notice');
 
 Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth:admin', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth:admin', 'throttle:6,1'])
                 ->name('verification.send');
 
 Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('password.confirm');
 
 Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth');
+                ->middleware('auth:admin');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('logout');
 
